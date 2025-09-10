3638954770cf494953e16d08094000d6b4f3d31b
shamhasan <ihtishamulhasaaan@gmail.com>
2025-09-10 08:01:10 +0700

    table article dah bener

--- database/migrations/2025_09_09_040445_create_articles_table.php
+++ database/migrations/2025_09_09_040445_create_articles_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+
+            // foreign key
+            $table->foreignId('author_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('users')->onDelete('cascade');
+
+            // Attribut title dan content
+            $table->string('title');
+            $table->longText('content');
+            $table->string('image_url')->nullable();
+
+            // status
+            $table->enum('status', ['published','draft']);
+
+            //timestamp
             $table->timestamps();
         });
     }
