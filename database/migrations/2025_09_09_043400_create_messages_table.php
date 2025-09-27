816d9f5eb0fb1ce38a537218643481fcc413484e
Fakhry224 <fakhryzahranhakim@gmail.com>
2025-09-27 09:57:08 +0700

    fix: database migration for konsultasi chatbot

--- database/migrations/2025_09_09_043400_create_messages_table.php
+++ database/migrations/2025_09_09_043400_create_messages_table.php
@@ -6,18 +6,13 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
 
-            //foreign key ke consultation
             $table->foreignId('consultation_id')->constrained('consultations')->onDelete('cascade');
 
-            //info messages
             $table->enum('sender_type', ['user', 'ai'])->default('user');
             $table->text('content');
             $table->timestamp('sent_at')->useCurrent();
@@ -26,9 +21,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('messages');
